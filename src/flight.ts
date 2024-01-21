fc7f55a78e7719710ffe5f55cb5f4742a4c24d41
Dan Stephenson <ispyhumanfly@gmail.com>
2024-01-21 10:34:01 -0500

    chore initial checking in of flight code.

---
+++ src/flight.ts
@@ -0,0 +1,108 @@
+#!ts-node
+
+import Koa from 'koa'
+import Router from '@koa/router'
+import fg from 'fast-glob'
+import path from 'path'
+import cors from '@koa/cors'
+import bodyParser from 'koa-bodyparser'
+import logger from 'koa-logger'
+import compress from 'koa-compress'
+import ratelimit from 'koa-ratelimit'
+import cluster from 'cluster'
+import os from 'os'
+import { exec } from 'child_process'
+import serve from 'koa-static'
+import cache from 'koa-redis-cache' // Import the middleware
+
+import Redis from 'ioredis'
+
+const argv = require('yargs/yargs')(process.argv.slice(2)).argv
+
+// change working directory of this script to ./src/
+process.chdir(path.resolve(__dirname, './src'))
+
+process
+if (!argv.mode) {
+    argv.mode = 'development'
+}
+
+console.log = console.log.bind(null, 'Flight:')
+
+if (cluster.isPrimary) {
+    const numCPUs = os.cpus().length
+
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork()
+    }
+
+    cluster.on('exit', () => {
+        cluster.fork()
+    })
+} else {
+    const app = new Koa()
+    const router = new Router()
+
+    if (argv.mode === 'production') {
+
+        exec('npx vite build', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`)
+                return
+            }
+            console.log(`stdout: ${stdout}`)
+            console.error(`stderr: ${stderr}`)
+        })
+
+        app.use(compress())
+        app.use(
+            ratelimit({
+                driver: 'redis',
+                db: new Redis(),
+                duration: 60000,
+                errorMessage: 'Sometimes You Just Have to Slow Down.',
+                id: (ctx) => ctx.ip,
+                headers: {
+                    remaining: 'Rate-Limit-Remaining',
+                    reset: 'Rate-Limit-Reset',
+                    total: 'Rate-Limit-Total'
+                },
+                max: 100,
+                disableHeader: false
+            })
+        )
+        app.use(cache({ expire: 30 /* Cache time in seconds */ }))
+        app.use(logger())
+        app.use(serve('../dist'))
+        console.log("App served out of dist/ and available on port 3000")
+    }
+
+    app.use(cors()).use(bodyParser())
+
+    const serverFiles = fg.sync('app/components/**/*.server.ts')
+    serverFiles.forEach((file) => {
+        const serverRoutes = require(path.resolve(file))
+        if (serverRoutes && serverRoutes.default) {
+            router.use(serverRoutes.default)
+        }
+    })
+
+    app.use(router.routes()).use(router.allowedMethods())
+
+    app.listen(3000, () => {
+        console.log(`Server worker ${process.pid} started, All services are running on port 3000`)
+    })
+
+    if (argv.mode === 'development') {
+        exec('npx vite', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`)
+                return
+            }
+            console.log(`stdout: ${stdout}`)
+            console.error(`stderr: ${stderr}`)
+        })
+
+        console.log(`Vite development server with hot module reload ${process.pid} started on 3001`)
+    }
+}
