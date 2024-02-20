dc02ac18fc3355b065cb534f4ee154166e7f70d5
Dan Stephenson <ispyhumanfly@gmail.com>
2024-02-20 14:03:06 -0500

    chore: Updated session config.

--- flight.ts
+++ flight.ts
@@ -55,15 +55,15 @@ if (cluster.isPrimary) {
         // Session configuration options
         // For example, to change the cookie name:
         key: 'koa:sess', // default
-        maxAge: 86400000, // cookie's expire time, 24 hours in milliseconds
-        autoCommit: true, /** (boolean) automatically commit headers (default true) */
-        overwrite: true, /** (boolean) can overwrite or not (default true) */
-        httpOnly: true, /** (boolean) httpOnly or not (default true) */
-        signed: true, /** (boolean) signed or not (default true) */
-        rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
-        renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
-        secure: false, /** (boolean) secure cookie*/
-        sameSite: null, /** (string) session cookie sameSite options (default null, don't set it) */
+        // maxAge: 86400000, // cookie's expire time, 24 hours in milliseconds
+        // autoCommit: true, /** (boolean) automatically commit headers (default true) */
+        // overwrite: true, /** (boolean) can overwrite or not (default true) */
+        // httpOnly: true, /** (boolean) httpOnly or not (default true) */
+        // signed: true, /** (boolean) signed or not (default true) */
+        // rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+        // renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+        // secure: false, /** (boolean) secure cookie*/
+        // sameSite: null, /** (string) session cookie sameSite options (default null, don't set it) */
         path: '/', /** (string) session cookie path */
         store: RedisStore({
             client: new Redis()
